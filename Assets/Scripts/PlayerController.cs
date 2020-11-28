9ea59b5dce0c15a7cb252192dbb63dfaab50976f
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-28 14:50:52 +0100

    Player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float fireForce;
+    [SerializeField] private float stickRadius;
+    [SerializeField] private LayerMask stickMask;
+    [SerializeField] private float stickCooldown;
+
+    private bool isSticking;
+    private float stickTimer;
+    private Vector2 aimDir;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        StickToWall();
+
+        if (isSticking) {
+            GetPlayerAim();
+        }
+
+        UpdateTimers();
+    }
+
+    private void StickToWall()
+    {
+        if (isSticking || stickTimer > 0f) {
+            return;
+        }
+
+        if (Physics2D.OverlapCircle(transform.position, stickRadius, stickMask)) {
+            isSticking = true;
+            rb.bodyType = RigidbodyType2D.Static;
+        }
+    }
+
+    private void GetPlayerAim()
+    {
+        if (Input.GetButton("Fire1")) {
+            Aim();
+        } else if (Input.GetButtonUp("Fire1")) {
+            Fire();
+        }
+    }
+
+    private void Aim()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        aimDir = (Vector2)transform.position - mousePos;
+    }
+
+    private void Fire()
+    {
+        isSticking = false;
+        stickTimer = stickCooldown;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+
+        rb.velocity = fireForce * aimDir;
+    }
+
+    private void UpdateTimers()
+    {
+        if (stickTimer > 0f) {
+            stickTimer -= Time.deltaTime;
+        }
     }
 }
