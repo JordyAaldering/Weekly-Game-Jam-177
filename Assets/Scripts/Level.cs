3ea901ddf08e2a3ea024899d240f7cf0aebcedb6
Jordy Aaldering <aalderingjordy@gmail.com>
2020-11-30 12:54:27 +0000

    Basic level loading from file

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName="New Level", menuName="Level")]
+public class Level : ScriptableObject
+{
+    [SerializeField] private int initialMoves;
+    public int InitialMoves => initialMoves;
+
+    [Tooltip("_: None\nw: Wall\np: Player\ne: End")]
+    [SerializeField, TextArea(10,20)] private string levelData;
+    private string levelDataFormat;
+
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+    public TileType[,] LevelGrid { get; private set; }
+
+    public void PopulateGrid()
+    {
+        levelDataFormat = levelData.ToLower();
+        string[] lines = levelDataFormat.Split(new[] { '\n' });
+
+        Width = lines[0].Length;
+        Height = lines.Length;
+        LevelGrid = new TileType[Width, Height];
+
+        for (int y = 0; y < Height; y++) {
+            string line = lines[y];
+            for (int x = 0; x < Width; x++) {
+                char c = line[x];
+                int dy = Height - y - 1;
+                LevelGrid[x, dy] = CharToTile(c);
+            }
+        }
+    }
+
+    private TileType CharToTile(char c)
+	{
+        switch (c) {
+            case '-': return TileType.None;
+            case 'w': return TileType.Wall;
+            case 'p': return TileType.Player;
+            case 'e': return TileType.End;
+        }
+
+        Debug.LogWarning($"unknown tile type '{c}'");
+        return TileType.None;
+    }
+}
+
+public enum TileType
+{
+    None,
+    Wall,
+    Player,
+    End,
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,21 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private int initialMoves;
-
     private static LevelManager instance;
     public static LevelManager Instance =>
         instance ?? (instance = FindObjectOfType<LevelManager>());
 
-    public bool IsGameOver { get; private set; }
+    [SerializeField] private Level level;
+    [SerializeField] private RuleTile wallTile;
+    [SerializeField] private Tilemap wallsTileMap;
+
+    private Transform player;
 
     public MoveManager MoveManager { get; private set; }
 
+    public bool IsGameOver { get; private set; }
+
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         MoveManager = FindObjectOfType<MoveManager>();
-        MoveManager.Initialize(initialMoves);
+
+        level.PopulateGrid();
+        SetupLevel(level);
+    }
+
+    private void SetupLevel(Level level)
+    {
+        MoveManager.Initialize(level.InitialMoves);
+
+        for (int y = 0; y < level.Height; y++) {
+            for (int x = 0; x < level.Width; x++) {
+                Vector3Int pos = new Vector3Int(x, y, 0);
+                switch (level.LevelGrid[x, y]) {
+                    case TileType.Wall:
+                        wallsTileMap.SetTile(pos, wallTile);
+                        break;
+                    case TileType.Player:
+                        player.position = pos;
+                        break;
+                }
+			}
+		}
     }
 
     public void GameOver()
